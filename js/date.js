4d904f34be429fc192828935877a8ca55bf434b7
hulett5757 <hul19021@byui.edu>
2022-04-23 20:12:01 -0600

    update

--- js/date.js
+++ js/date.js
@@ -1,2 +1,1 @@
-const options = {day: 'numeric', month: 'long', year: 'numeric'}
-document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+document.getElementById("modify").innerHTML = document.lastModified 
