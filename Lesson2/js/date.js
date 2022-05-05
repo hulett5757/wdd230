4b0062606ecdd78c65f76fb63612876e42050d99
hulett5757 <81761901+hulett5757@users.noreply.github.com>
2022-05-05 09:04:18 -0600

    update

--- Lesson2/js/date.js
+++ Lesson2/js/date.js
@@ -1,10 +1,5 @@
-let dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-let monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
 let d = new Date();
-let dayName = dayNames[d.getDay()];
-let monthName = monthNames[d.getMonth()];
 let year = d.getFullYear();
-let fulldate = dayName + ","  +" " + monthName + " " + d.getDate() + "," + " " + year;
-document.getElementById(`currentdate`).textContent = fulldate
+let fulldate = year;
+document.getElementById(`currentyear`).textContent = fulldate
 document.querySelector('#lastUpdate').innerHTML = `Last Updated: ${document.lastModified}`
