2317d1afa73b651047710054a00598a1b88ef149
hulett5757 <81761901+hulett5757@users.noreply.github.com>
2022-06-02 09:59:54 -0600

    update

--- lazyload/js/progressiveimage.js
+++ lazyload/js/progressiveimage.js
@@ -1,8 +1,8 @@
 const imagesToLoad = document.querySelectorAll("[data-src]");
 
 const imgOptions = {
-    threshold: 0,
-    rootMargin: "0px 0px 50px 0px"
+    threshold: 1,
+    rootMargin: "0px 0px -500px 0px"
 };
 
 const loadImages = (image) => {
