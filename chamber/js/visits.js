f816f61d896e3e9dfb3c6899e1cbae021a494337
hulett5757 <81761901+hulett5757@users.noreply.github.com>
2022-06-06 20:33:09 -0600

    update

--- chamber/js/visits.js
+++ chamber/js/visits.js
@@ -17,14 +17,18 @@ if (numVisits !== 0) {
 numVisits++;
 // store the new number of visits value
 localStorage.setItem("visits-ls", numVisits);
+ 
+let visit1 = (Math.floor(Date.now()/(1000*60*60*24)));
+
+let visit2 = new Date();
+
+let timeDiff = (visit2 - visit1);
 
 // show todays date.
 todayDisplay.textContent = Date.now();
 
-daysDisplay.textContent = (Math.round(Date.now()/(1000*60*60*24)/86000));
+daysDisplay.textcontent = Math.floor(timeDiff);
 
+/*daysDisplay.textContent = (Math.floor(Date.now()/(1000*60*60*24)));*/
 
 dateDisplay.textContent = new Date();
-
-
-
