3b0f5d8919ddc0caa61b230b0cd3507b63c30423
hulett5757 <81761901+hulett5757@users.noreply.github.com>
2022-06-15 16:08:36 -0600

    update

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -1,32 +1,32 @@
-const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+const jsonData= require('./json/directory.json');
 const cards = document.querySelector('.cards');
 
-fetch(requestURL)
+fetch(jsonData)
   .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
     console.table(jsonObject);  // temporary checking for valid response and data parsing
-    const prophets = jsonObject['prophets'];
-    prophets.forEach(displayProphets);
+    const business = jsonObject['business'];
+    business.forEach(displayBusiness);
   });
 
   function displayProphets(prophet) {
     // Create elements to add to the document
-    let card = document.createElement('section');
+    let card = document.createElement('section.b');
     let h2 = document.createElement('h2');
     let h3 = document.createElement('h3');
     let h4 = document.createElement('h4');
     let portrait = document.createElement('img');
   
     // Change the textContent property of the h2 element to contain the prophet's full name
-    h2.textContent = `${prophet.name} ${prophet.lastname}`
+    h2.textContent = `${business.name}`
 
     //Change the textContent property of the h3 element to contain the prophets birthdate
-    h3.textContent = `Date of Birth ${prophet.birthdate}`
+    h3.textContent = `${business.address}`
 
     //Change the textContent property of the h3 element to contain the prophets birthplace
-    h4.textContent = `Place of Birth ${prophet.birthplace}`
+    h4.textContent = `${business.phone}`
   
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
     portrait.setAttribute('src', prophet.imageurl);
