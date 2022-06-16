110f02e48eba8bc6d714602c37a88bb9152651a4
hulett5757 <81761901+hulett5757@users.noreply.github.com>
2022-06-16 09:01:23 -0600

    update

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -17,7 +17,7 @@ fetch(requestURL)
     let h2 = document.createElement('h2');
     let h3 = document.createElement('h3');
     let h4 = document.createElement('h4');
-    /*let portrait = document.createElement('img');*/
+    let icon = document.createElement('img');
   
     // Change the textContent property of the h2 element to contain the prophet's full name
     h2.textContent = `${business.name}`
@@ -29,15 +29,14 @@ fetch(requestURL)
     h4.textContent = `${business.phone}`
   
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-    /*portrait.setAttribute('src', prophet.imageurl);
-    portrait.setAttribute('alt', `Portait of ${prophet.name} ${prophet.lastname} ${prophet.order} Latter-day President`);
-    portrait.setAttribute('loading', 'lazy');*/
+    icon.setAttribute('src', business.imageurl);
+    icon.setAttribute('alt', `Icon for ${business.name}`);
   
     // Add/append the section(card) with the h2 element
     card.appendChild(h2);
     card.appendChild(h3);
     card.appendChild(h4);
-    /*card.appendChild(portrait);*/
+    card.appendChild(icon);
   
     // Add/append the existing HTML div with the cards class with the section(card)
     document.querySelector('div.cards').appendChild(card);
