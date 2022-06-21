d474d811e98415b6dc9dd83f46dd1b44077a0c72
hulett5757 <81761901+hulett5757@users.noreply.github.com>
2022-06-21 09:40:33 -0600

    update

--- chamber/js/weatherapi.js
+++ chamber/js/weatherapi.js
@@ -1,18 +1,48 @@
 // select HTML elements to edit
-const currentTemp = document.querySelector('#current-temp');
+/*const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = "https://api.openweathermap.org/data/2.5/weather?id={5861896}&appid={8017c1d5aea32b6094764ab6d12a29de}";
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&appid=8017c1d5aea32b6094764ab6d12a29de&units=imperial";
 fetch(url)
   .then((response) => response.json())
   .then((data) => {
-    console.log(data); // this is temporary for development only
     currentTemp.innerHTML = `<strong>${data.main.temp.toFixed(0)}</strong>`;
-  });
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    const desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+  });*/
 
-const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-const desc = data.weather[0].description;
+  // select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&appid=8017c1d5aea32b6094764ab6d12a29de&units=imperial";
 
-weatherIcon.setAttribute('src', iconsrc);weatherIcon.setAttribute('alt', desc);
-captionDesc.textContent = desc;
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      displayResults(data);
+    } else {
+        throw Error(await response.text());
+    }
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+apiFetch();
+
+function  displayResults(weatherData) {
+  currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+
+  const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+  const desc = weatherData.weather[0].description;
+
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = desc;
+}
