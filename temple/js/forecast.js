f50d3b06404f1ca526d3dbcdabde5d0779d1a844
hulett5757 <81761901+hulett5757@users.noreply.github.com>
2022-07-19 09:24:49 -0600

    update

--- temple/js/forecast.js
+++ temple/js/forecast.js
@@ -1,36 +1,41 @@
+const forecastEl = document.querySelector("#forecast");
+console.log(forecastEl)
 fetchForecast = function () {
 	let endpoint =
        "https://api.openweathermap.org/data/2.5/onecall?lat=42.3601&lon=-71.0589&exclude=current,hourly,minutely,alerts&units=imperial&appid=8017c1d5aea32b6094764ab6d12a29de";
-	const forecastEl = document.getElementsByClassName("forecast");
-    console.log(forecast);
+	
 
 	fetch(endpoint)
 	.then(function (response) {
 		if (200 !== response.status) {
-			console.log(
 				"Looks like there was a problem. Status Code: " + response.status
-			);
 			return;
 		}
+		
 
-		forecastEl[0].classList.add('loaded');
+		/*forecastEl[0].classList.add('loaded');*/
 
 		response.json().then(function (data) {
-			const fday = "";
-			data.daily.forEach((value, index) => {
+			let fday = "";
+			console.log(data)
+			data.daily.slice(0, 4).forEach((value, index) => {
 				if (index > 0) {
 					const dayname = new Date(value.dt * 1000).toLocaleDateString("en", {
 						weekday: "long",
 					});
 					const icon = value.weather[0].icon;
+					const iconsrc = `https://openweathermap.org/img/w/${value.weather[0].icon}.png`;
+					console.log(icon)
 					const temp = value.temp.day.toFixed(0);
+					console.log(temp)
 					fday = `<div class="forecast-day">
 						<p>${dayname}</p>
-						<p><span class="ico-${icon}" title="${icon}"></span></p>
+						<img src=${iconsrc}>
 						<div class="forecast-day--temp">${temp}<sup>°C</sup></div>
 					</div>`;
-					forecastEl[0].insertAdjacentHTML('beforeend', fday);
-                    console.log();
+					console.log(fday)
+					forecastEl.innerHTML +=fday
+
 				}
 			});
 		});
@@ -39,3 +44,4 @@ fetchForecast = function () {
 		console.log("Fetch Error :-S", err);
 	});
 };
+fetchForecast()
